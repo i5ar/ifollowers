4709fd317f69712ecd31460720e514b084bcf15f
iSar <i5ar@live.it>
2015-05-03 13:55:02 +0200

    Update main_dev.py

--- main_dev.py
+++ main_dev.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 ifollowers_info = {
-    "name": "ifollowers",
+    "name": "iFollowers",
     "author": "iSar",
     "version": (0, 0, 6),
     "python": (3, 4),
@@ -15,6 +15,7 @@ import glob
 import markdown
 import tweepy
 import sqlite3
+import ioauth
 
 owner = ''
 # Check database
@@ -29,13 +30,15 @@ if os.path.isfile('ifollowers.db'):
     conn.close()
     newner = input('Enter \''+ owner +'\' or a new Owner: ')
 else:
+    print('Please go to https://apps.twitter.com/ to Create New App;')
+    print('Open Permissions tab and check Read only;')
+    print('Open Keys and Access Token tab to copy your Application Settings.')
+
     newner = input('Owner: ')
 
 if(newner != owner):
 
     # Dynamic Twitter OAuth
-    import ioauth
-
     isar_dynamic_oauth = ioauth.DynamicTwitterOAuth()
 
     #owner = isar_dynamic_oauth.owner
@@ -78,8 +81,7 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-'''
-# Print followers
+'''Print followers
 for follower in api.followers_ids(owner):
     print(api.get_user(follower).screen_name)
 '''
@@ -93,8 +95,7 @@ for follower in tweepy.Cursor(api.followers, screen_name=owner).items():
 last_ls = ['Leonardo', 'Raffaello', 'Donatello', 'Michelangelo']
 '''
 
-'''
-# Get Specific markdown file in case you'd like a specific last_ls
+'''Get Specific markdown file in case you'd like a specific last_ls
 with open("_build\\markdown\\2014-12-27T175811.md", "rt") as in_md:
     last_followers = in_md.read()
     last_ls = last_followers.split()
@@ -108,8 +109,8 @@ if not os.path.exists('_build\\html'):
 if not os.path.exists('_build\\dat'):
     os.makedirs('_build\\dat')
 
-# Representing a time [4]
-# [4]: https://docs.python.org/3.0/library/time.html#time.strftime
+# Time access and conversions [1]
+# [1]: https://docs.python.org/3.0/library/time.html#time.strftime
 date_name = time.strftime("%Y-%m-%dT%H%M%S")
 only_date_name = time.strftime("%Y-%m-%d")
 date_name_path = '_build\\dat\\'+date_name+'.dat'
@@ -138,41 +139,41 @@ with open(date_name_path, "wt") as out_md:
         out_md.write(i+'\n')
     out_md.write('\n')
 
-# Write last followers in markdown file
-with open("_build\\markdown\\iFollowers.md", "wt") as out_md:
+#Write last followers in markdown file
+with open("_build\\markdown\\last.md", "wt") as out_md:
     head( 'Last followers', last_ls )
     followers( last_ls )
 
-# Get a list of files in data directory and sort last modified
+# List data files
 dic = {}
 for file in glob.glob("_build\\dat\\*.dat"):
     key = int(os.path.getmtime(file))   # Key
     dic[key] = file                     # Value
-
+# List data files
 ls = []
 for key in dic.keys():
     ls.append(key)
+# Sort data files
 sorted_ls = sorted(ls)
-#print('lengh: '+str(len(ls)))
 
+# Check if there are data files to compare in order to list first followers, new followers and unfollowers
 if len(ls) > 1:
     # Get first data file from list
-    first = dic[sorted_ls[0]]
+    first_data = dic[sorted_ls[0]]
     # Get last data file from list
-    last = dic[sorted_ls[-1]]
+    last_data = dic[sorted_ls[-1]]
     # Opening the older markdown file to read the old followers
-    with open(first, "rt") as in_md:
+    with open(first_data, "rt") as in_md:
         first_followers = in_md.read()
-        #print(first_followers)
 
-    with open("_build\\markdown\\iFollowers.md", "at") as out_md:
-
-        # Append first followers
+    # Write first followers markdown list
+    with open("_build\\markdown\\first.md", "wt") as out_md:
         first_ls = first_followers.split()
         head( 'First followers', first_ls )
         followers( first_ls )
 
-        # Append new followers
+    # Write new followers markdown list
+    with open("_build\\markdown\\new.md", "wt") as out_md:
         new_followers_ls = []
         for last in last_ls:
             if not last in first_ls:
@@ -180,7 +181,8 @@ if len(ls) > 1:
         head( 'New followers', new_followers_ls )
         followers( new_followers_ls )
 
-        # Append unfollowers
+    # Write unfollowers markdown list
+    with open("_build\\markdown\\un.md", "wt") as out_md:
         unfollowers_ls = []
         for first in first_ls:
             if not first in last_ls:
@@ -188,30 +190,8 @@ if len(ls) > 1:
         head( 'Unfollowers', unfollowers_ls )
         followers( unfollowers_ls )
 
-# Check if there are at least 2 data files to compare in order to list first followers, new followers and unfollowers
-if len(ls) > 1:
-    # Write last followers markdown list
-    with open("_build\\markdown\\last.md", "wt") as out_md:
-        head( 'Last followers', last_ls )
-        followers( last_ls )
-
-    # Write first followers markdown list
-    with open("_build\\markdown\\first.md", "wt") as out_md:
-        head( 'First followers', first_ls )
-        followers( first_ls )
-
-    # Write new followers markdown list
-    with open("_build\\markdown\\new.md", "wt") as out_md:
-        head( 'New followers', new_followers_ls )
-        followers( new_followers_ls )
-
-    # Write unfollowers markdown list
-    with open("_build\\markdown\\un.md", "wt") as out_md:
-        head( 'Unfollowers', unfollowers_ls )
-        followers( unfollowers_ls )
-
 else:
-    # Write empty last followers markdown list
+    # Write last followers markdown list
     with open("_build\\markdown\\last.md", "wt") as out_md:
         head( 'Followers', last_ls )
         followers( last_ls )
@@ -244,8 +224,7 @@ html_header = '<!DOCTYPE html>' \
 html_footer = '</body>' \
               '</html>'
 
-# Markdown Details [2]
-# [2]: http://bit.ly/1weDnEg
+# Generate markdown
 with open("_build\\markdown\\last.md", 'rt') as in_md:
     last = in_md.read()
     html_last = markdown.markdown(last)
@@ -259,21 +238,14 @@ with open("_build\\markdown\\un.md", 'rt') as in_md:
     un = in_md.read()
     html_un = markdown.markdown(un)
 
-'''
-#in_md = codecs.open("_build/markdown/ifollowers.md", mode="r", encoding="utf-8")
-with open("_build\\markdown\\iFollowers.md", 'rt') as in_md:
-    iFollowers = in_md.read()
-    html_page = markdown.markdown(iFollowers)
-'''
-
 # Build the page
 html_page = '<div class="col-md-3">'+ html_last +'</div>' \
             '<div class="col-md-3">'+ html_first +'</div>' \
             '<div class="col-md-3">'+ html_new +'</div>' \
             '<div class="col-md-3">'+ html_un +'</div>'
 
-with open("_build\\html\\"+only_date_name+"_iFollowers.html", "wt") as out_html:
+with open("_build\\html\\"+only_date_name+"_ifollowers.html", "wt") as out_html:
     out_html.write(html_header + html_page + html_footer)
 
 # Open the file in a browser
-os.system(cwd + "\\_build\\html\\"+only_date_name+"_iFollowers.html")
+os.system(cwd + "\\_build\\html\\"+only_date_name+"_ifollowers.html")
